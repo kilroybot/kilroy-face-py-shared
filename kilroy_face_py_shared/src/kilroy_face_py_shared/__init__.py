3db9d98bde078db6141561c4a3966ddb31071d31
Sebastian Pietras <01133337@pw.edu.pl>
2022-08-18 15:47:48 +0200

    Moved to `gRPC` (#4)
    Moved to gRPC

--- kilroy_face_py_shared/src/kilroy_face_py_shared/__init__.py
+++ kilroy_face_py_shared/src/kilroy_face_py_shared/__init__.py
@@ -1,25 +1,53 @@
-from kilroy_face_py_shared.models import (
-    BaseFaceModel,
-    Config,
-    ConfigNotification,
-    ConfigSchema,
-    ConfigSetReply,
-    ConfigSetRequest,
-    JSONSchema,
-    PostReply,
+from kilroy_face_py_shared.errors import (
+    AppError,
+    INTERNAL_SERVER_ERROR,
+    INVALID_CONFIG_ERROR,
+    PARAMETER_GET_ERROR,
+    PARAMETER_SET_ERROR,
+    STATE_NOT_READY_ERROR,
+)
+from kilroy_face_py_shared.messages import (
+    GeneratedPost,
+    GetConfigRequest,
+    GetConfigResponse,
+    GetConfigSchemaRequest,
+    GetConfigSchemaResponse,
+    GetMetadataRequest,
+    GetMetadataResponse,
+    GetPostSchemaRequest,
+    GetPostSchemaResponse,
+    GetStatusRequest,
+    GetStatusResponse,
     PostRequest,
-    PostSchema,
-    ScoreReply,
+    PostResponse,
+    RealPost,
     ScoreRequest,
-    ScrapReply,
+    ScoreResponse,
     ScrapRequest,
+    ScrapResponse,
+    SetConfigRequest,
+    SetConfigResponse,
     Status,
-    StatusEnum,
-    StatusNotification,
+    WatchConfigRequest,
+    WatchConfigResponse,
+    WatchStatusRequest,
+    WatchStatusResponse,
+)
+from kilroy_face_py_shared.metadata import Metadata
+from kilroy_face_py_shared.models import SerializableModel
+from kilroy_face_py_shared.posts import (
+    BasePost,
+    ImageData,
+    ImageOnlyPost,
+    ImageWithOptionalTextPost,
+    TextAndImagePost,
+    TextData,
+    TextOnlyPost,
+    TextOrImagePost,
+    TextWithOptionalImagePost,
 )
 from kilroy_face_py_shared.resources import (
     resource,
     resource_bytes,
     resource_text,
 )
-from kilroy_face_py_shared.types import JSON
